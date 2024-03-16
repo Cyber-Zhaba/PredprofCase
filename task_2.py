2076c4cac1b88d268be22890309608f3c865aa85
kids01 <20096ninja@gmail.com>
2024-03-16 14:31:55 +0300

    Task 2

---
+++ task_2.py
@@ -0,0 +1,32 @@
+import csv
+
+
+def sort(lst):
+    """List inplace Sort in O(N2) complexity
+
+    Keyword arguments:
+    lst -- list of ShipNames
+    """
+    n = len(lst)
+    for i in range(n):
+        for j in range(i + 1, n):
+            if int(lst[i].split('-')[1]) > int(lst[j].split('-')[1]):
+                lst[i], lst[j] = lst[j], lst[i]
+
+
+# Чтение исходного файла
+with open("space.txt", "r", encoding="utf-8") as csvfile:
+    # Формат переменной data: list[dict[str, str]]
+    data = list(csv.DictReader(csvfile, delimiter="*"))
+
+    # Получаем именна кораблей
+    names = [data[i]["ShipName"] for i in range(len(data))]
+
+    # Сортируем
+    # Функция sort() работает inplace
+    # То есть все операции изменений проводятся на исходном списке
+    sort(names)
+
+    # Выводим первые десять названий
+    # Разделитель - новая строка
+    print(*names[:10], sep="\n")
